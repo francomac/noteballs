26e461ef294dd0f751552e62a437d7448bd5b29f
Franco Martinez <franco.mcarballo@outlook.com>
2023-11-16 17:46:40 -0600

    adding new notes through form

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -4,22 +4,31 @@
       <div class="field">
         <label class="label">Message</label>
         <div class="control">
-          <textarea class="textarea" placeholder="Textarea"></textarea>
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Textarea"
+            ref="newNoteRef"
+          ></textarea>
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            @click="addNewNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
             Add New Card
           </button>
         </div>
       </div>
     </div>
 
-    <div class="card mb-3">
-      <header class="card-header">
-        <p class="card-header-title">Component</p>
+    <div v-for="note in notes" :key="note.id" class="card mb-3">
+      <header class="is-hidden card-header">
+        <p class="card-header-title">Nota {{ note.id }}</p>
         <button class="card-header-icon" aria-label="more options">
           <span class="icon">
             <i class="fas fa-angle-down" aria-hidden="true"></i>
@@ -28,10 +37,7 @@
       </header>
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
-          totam? Enim, quo voluptates laborum totam, eaque, fugiat dolore ab
-          inventore amet excepturi eligendi natus assumenda voluptas dolor?
-          Perferendis, ipsam iure.
+          {{ note.content }}
         </div>
       </div>
       <footer class="card-footer">
@@ -41,3 +47,33 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import uuid4 from 'uuid4'
+
+const newNote = ref(null)
+const newNoteRef = ref(null)
+
+const notes = ref([
+  {
+    id: 'NT1',
+    content: 'some text...'
+  },
+  {
+    id: 'NT2',
+    content: 'some some text...'
+  }
+])
+
+const addNewNote = () => {
+  let note = {
+    id: 'NT' + uuid4(),
+    content: newNote.value
+  }
+
+  notes.value.unshift(note)
+  newNote.value = null
+  newNoteRef.value.focus()
+}
+</script>
