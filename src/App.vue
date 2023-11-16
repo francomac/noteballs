47e74572a4179662e51f485ec99cd54fc2bb0960
Franco Martinez <franco.mcarballo@outlook.com>
2023-11-15 21:39:03 -0600

    bulma css framework added to project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> |
-  <RouterLink to="stats">Stats</RouterLink>
+  <RouterLink to="/" class="button">Notes</RouterLink> |
+  <RouterLink to="stats" class="button">Stats</RouterLink>
   <router-view></router-view>
 </template>
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
